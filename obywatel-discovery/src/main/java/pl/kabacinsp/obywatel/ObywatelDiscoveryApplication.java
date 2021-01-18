7b89ea69672c2e9bc98c84d511592716cd90c9dc
kabacinsp <50554080+kabacinsp@users.noreply.github.com>
2021-01-18 18:20:37 +0100

    Create registry service

---
+++ obywatel-discovery/src/main/java/pl/kabacinsp/obywatel/ObywatelDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package pl.kabacinsp.obywatel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ObywatelDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObywatelDiscoveryApplication.class, args);
+	}
+
+}
