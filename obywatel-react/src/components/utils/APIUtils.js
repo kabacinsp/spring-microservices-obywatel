8a3270fc196bdea97888c8926c4266fd4be41dcd
kabacinsp <kabacinskip@gmail.com>
2021-01-21 19:53:59 +0100

    Create signup page

---
+++ obywatel-react/src/components/utils/APIUtils.js
@@ -0,0 +1,36 @@
+import { API_BASE_URL } from '../common/constants';
+
+const request = (options) => {
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+    })
+
+    const defaults = {headers: headers};
+    options = Object.assign({}, defaults, options);
+
+    return fetch(options.url, options)
+    .then(response =>
+        response.json().then(json => {
+            if(!response.ok) {
+                return Promise.reject(json);
+            }
+            return json;
+        })
+    );
+};
+
+export function login(loginRequest) {
+    return request({
+        url: API_BASE_URL + "/auth/login",
+        method: 'POST',
+        body: JSON.stringify(loginRequest)
+    });
+}
+
+export function signup(signupRequest) {
+    return request({
+        url: API_BASE_URL + "/auth/signup",
+        method: 'POST',
+        body: JSON.stringify(signupRequest)
+    });
+}
