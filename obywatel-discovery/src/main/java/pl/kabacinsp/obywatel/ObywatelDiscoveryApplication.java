febbb9f60fccc5e70bcd9a6cb4fc0647086c4ca6
kabacinsp <kabacinskip@gmail.com>
2021-01-23 15:55:27 +0100

    Add dependency to pom file

--- obywatel-discovery/src/main/java/pl/kabacinsp/obywatel/ObywatelDiscoveryApplication.java
+++ obywatel-discovery/src/main/java/pl/kabacinsp/obywatel/ObywatelDiscoveryApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class ObywatelDiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ObywatelDiscoveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ObywatelDiscoveryApplication.class, args);
+    }
 
 }
