3875ce741056a57651d9a8147ffd11363d422047
kabacinsp <kabacinskip@gmail.com>
2021-01-25 20:30:50 +0100

    Configure auth gateway

--- obywatel-auth/src/main/java/pl/kabacinsp/obywatel/config/WebSecurityConfig.java
+++ obywatel-auth/src/main/java/pl/kabacinsp/obywatel/config/WebSecurityConfig.java
@@ -45,7 +45,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login*").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login");
 //            .authorizeRequests()
 //            .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
 //                .anyRequest().authenticated()
